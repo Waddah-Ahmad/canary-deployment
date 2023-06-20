8db90e051e86ba257d07d9f15b58e02a8e2b5ba3
Nihat Alim <iletisim@nihatalim.com.tr>
2023-06-20 23:44:03 +0300

    producer initiated

---
+++ producer/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import "github.com/spf13/viper"
+
+type AppConfig struct {
+	Kafka KafkaConfig
+}
+
+type KafkaConfig struct {
+	Brokers  string
+	Topic    string
+	ClientId string
+}
+
+func GetConfig() *AppConfig {
+	var appConfig AppConfig
+
+	v := viper.New()
+	v.SetConfigFile("resources/config.yml")
+
+	if err := v.ReadInConfig(); err != nil {
+		panic("error while reading configuration")
+	}
+
+	err := v.Unmarshal(&appConfig)
+	if err != nil {
+		panic("error while marshalling configuration")
+	}
+
+	return &appConfig
+}
